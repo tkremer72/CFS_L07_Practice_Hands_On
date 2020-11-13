da3cc8e48bac509e0dc3951b1a7fefbd1d6f241d
tbone7243 <tbone7243@gmail.com>
2020-11-13 18:22:51 -0500

    Added code to empty boiler place script.js file.

---
+++ script.js
@@ -0,0 +1,9 @@
+var awesomeBirds = ["Bluebirds","Road Runner", "Desert Dove", "Pigeon", ];
+           for (var i = 0; i < awesomeBirds.length; i++) {    
+            if (awesomeBirds[i] === "Bluebirds") {
+            alert("Bluebirds are so beautiful");
+    }
+    else {
+        alert("Not not as beautiful as bluebirds");
+    }
+}
